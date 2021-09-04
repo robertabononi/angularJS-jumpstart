b37c269a7ae51280c35bb4d4471eefc21898eebc
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2021-09-03 22:24:51 -0300

    chore: add a ng-view to the index page

--- app/app.js
+++ app/app.js
@@ -1,1 +1,11 @@
 let CostumersModule = angular.module('CostumersModule', ['ngRoute']);
+
+CostumersModule.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        controller: 'CostumersController',
+        templateUrl: '../app/views/costumers.html'
+    })
+    .otherwise( {
+        redirectTo: '/'
+    })
+})
